ede726b5e85f59ada3c183c77ed7b30d8abca61c
Akxhith007 <147645042+Akxhith007@users.noreply.github.com>
2023-10-14 18:51:36 +0530

    Add files via upload

---
+++ day3/index.js
@@ -0,0 +1,67 @@
+//prompt("enter your name");
+//console.log("this is console");
+//console.log("6+5");
+//document.write("<h1> javascript </h1>");
+
+// var num1=5;
+// var num2=6;//we can redeclare the variable
+// num1=8// we can reasssign the variable
+// console.log(num1);
+
+// let num2=9
+// //let num2=10// we cant redeclare the variable
+// num2=11// we can reassign the variables
+// console.log(num2);
+
+// const num3=10
+// //const num3=11//we cant redeclare the variable
+// //num3=15// we cant reassign the variable
+// console.log(num3);
+
+// console.log(3**4);
+// let x=5
+// x++
+// console.log(x);
+// x--
+// console.log(x);
+
+// let num1=5
+// let num2="5"
+// console.log(num1==num2);
+// console.log(num1==num2);
+
+// function mul(n1,n2,n3,n4){
+//     return n1*n2*n3*n4;
+// }
+// document.write(mul(4,5,6,7))
+// function greet(){
+//     alert("hello everyone");
+// }
+
+
+// let age=10
+// if(age>=18){
+//     document.write("eligible for vote");
+// }else{
+//     document.write("not eligible ");
+// }
+
+// for(let num=10;num>=1;num--){
+//     console.log(num);
+
+// }
+
+// Dom - Document Object Model
+// Dom Manipulation Methods
+// document.getElementsById()
+// document.getElementsByClassName()
+// document.getElementsByTagName()
+// document.querySelector()
+// document.querySelectorAll()
+
+
+// let el=document.getElementsById("head").innerHTML;
+// console.log(el);
+
+let ele=document.querySelector("#head").innerHTML;
+console.log(ele);
